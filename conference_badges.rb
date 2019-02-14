210eb5140e64f0075dcc2382c4c8dd017289508e
Ashley Archibald <aarchibald20@gmail.com>
2019-02-14 23:01:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,18 @@ def batch_badge_creator(names)
       badge_maker(name)
   end
 end
+
+def assign_rooms(speakers)
+  speakers.collect.each_with_index do |speaker, index|
+  "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
